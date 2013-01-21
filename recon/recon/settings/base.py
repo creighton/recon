847db1acd0ff0ff7277287882e4c695f8aafcf4c
tom creighton <tjcreighton@gmail.com>
2013-01-21 18:40:20 -0500

    adding urls and view stuff.. supervisor not working

--- recon/recon/settings/base.py
+++ recon/recon/settings/base.py
@@ -196,6 +196,7 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'connecttolrs',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
